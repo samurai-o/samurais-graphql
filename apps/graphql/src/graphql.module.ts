d85f04c5fa7786c700466b082c97bd54e05f0bbd
刘伟 <liuwei03@dongfangfuli.com>
2021-07-11 22:35:09 +0800

    fix: 访问白名单

--- apps/graphql/src/graphql.module.ts
+++ apps/graphql/src/graphql.module.ts
@@ -19,9 +19,11 @@ import { OrganizationResolver } from './organization/organization.resolver';
           introspection: true,
           playground: true,
           cacheControl: true,
+          debug: true,
+          transformAutoSchemaFile: true,
           cors: {
             credentials: true,
-            origin: 'https://studio.apollographql.com',
+            origin: true,
           },
           context: ({ req, res }) => {
             return {
@@ -40,4 +42,4 @@ import { OrganizationResolver } from './organization/organization.resolver';
   ],
   providers: [OrganizationResolver],
 })
-export class GraphqlModule { }
+export class GraphqlModule {}
--- apps/graphql/src/main.ts
+++ apps/graphql/src/main.ts
@@ -1,12 +1,26 @@
 import { ConfigurationService } from '@app/configuration';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Request } from 'express';
 import { GraphqlModule } from './graphql.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GraphqlModule);
   const config = app.get(ConfigurationService);
   const port = config.getEnvironment('GRPHQL_PORT') as string;
+  const allowlist = [
+    'https://www.samurais.cn',
+    'https://studio.apollographql.com',
+  ];
+  app.enableCors((req: Request, callback) => {
+    const corsOptions = {};
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions['origin'] = true;
+    } else {
+      corsOptions['origin'] = false;
+    }
+    callback(null, corsOptions);
+  });
   await app.listen(port, () => {
     Logger.log('auth', '应用');
     Logger.log(port, '端口');
