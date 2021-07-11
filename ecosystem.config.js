ad62c8772478e0887964039eaf265a99174a86af
刘伟 <liuwei03@dongfangfuli.com>
2021-07-11 21:43:52 +0800

    feat: graphql 启动项

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,6 +18,24 @@ module.exports = {
         NODE_ENV: 'development',
       },
     },
+    {
+      name: 'graphql',
+      namespace: 'samurai',
+      script: 'dist/apps/graphql/main.js',
+      mode: 'cluster',
+      instances: 2,
+      autorestart: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      out_file: 'logs/pm2/graphql.log',
+      error_file: 'logs/pm2/error_graphql.log',
+      max_memory_restart: '1024M',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_dev: {
+        NODE_ENV: 'development',
+      },
+    },
   ],
   deploy: {
     production: {
